8d2484935b8a712595c8dd95b3cc9e048951f26f
cleber <contatoclebercarvalho@gmail.com>
2020-07-15 12:27:42 -0300

    chore: configure toastify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ToastContainer />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
